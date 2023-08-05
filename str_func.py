bb90cf431161f60123a66ecd6ea12173875ef36c
morusgit <morus@bk.ru>
2023-08-06 00:23:09 +0300

    Add def upper_first_letters and doc strings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def upper_case(string):
     return string.upper()
+
+def upper_first_letters(string):
+    """
+    принимает строку на вход
+    заменяет первые буквы каждого слова в строке на заглавные
+    """
+    words = string.split()
+    upper_words = []
+    for word in words:
+        upper_word = word[1:] + word[0].upper()
+        upper_words.append(upper_word)
+    return ' '.join(upper_words)
